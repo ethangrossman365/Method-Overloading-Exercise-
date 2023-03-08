3babdf48e2a9256b0ab88f10d2ed5bf75fce07fc
ethan grossman <ethangrossman3@gmail.com>
2023-03-07 22:27:27 -0600

    finished exercise

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool withDollars)
+        {
+            if (withDollars && (a + b) != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (withDollars && (a + b) == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+
+        }
+
+
+        
+
+            
+
+    }   
+}
+
+
+
+
